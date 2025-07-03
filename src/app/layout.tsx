1e3a09a1bec8e1bc58b973bb2be2b7a446f3e62f
Jeff Lunt <jeff@opkie.com>
2025-07-03 21:25:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className="font-body antialiased min-h-screen bg-background">
+      <body className="font-body antialiased min-h-screen bg-background" suppressHydrationWarning={true}>
         {children}
         <Toaster />
       </body>
